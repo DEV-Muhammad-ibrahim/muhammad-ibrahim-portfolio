c6cfa164ff330b5d7342d0a4eb24e57b277ea1e0
DEV-Muhammad-ibrahim <ibrahim97416@gmail.com>
2024-05-16 18:04:19 +0500

    blog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="bg-slate-950 text-slate-100">
+    <html lang="en" className="bg-slate-950 text-slate-100 last:">
       <body className={clsx(urbanist.className, "relative min-h-screen")}>
         <Header />
         {children}
